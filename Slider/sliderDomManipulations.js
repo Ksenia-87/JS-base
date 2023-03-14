34666fce806971dc6dd1b48b4438d043b314f6fd
deprof@rambler.ru <deprof@rambler.ru>
2023-03-14 12:58:40 +0100

    fix delay issue

--- Slider/sliderDomManipulations.js
+++ Slider/sliderDomManipulations.js
@@ -3,20 +3,35 @@ const right = document.querySelector("#right");
 const items = document.querySelector("#items");
 const computed = window.getComputedStyle(items);
 
+let treshhold;
+
 right.addEventListener("click", function(e) {
   e.preventDefault();
-  let currentRight = parseInt(computed.right);
+  //let currentRight = parseInt(computed.right);
+
+if (treshhold) return;
+treshhold - true;
+
+let currentRight = parseInt(computed.right);
 
   if (currentRight < 500) {
     items.style.right = `${currentRight + 100}px`;
   }
+  setTimeout (() => {
+    treshhold = false; 
+  }, 300);
 });
 
 left.addEventListener("click", function(e) {
   e.preventDefault();
+  if (treshhold) return;
+  treshhold = true;
   let currentRight = parseInt(computed.right);
 
   if (currentRight > 0) {
     items.style.right = `${currentRight - 100}px`;
   }
+  setTimeout (() => {
+    treshhold = false; 
+  }, 300);
 });
